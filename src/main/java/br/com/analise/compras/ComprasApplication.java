8814d5ee826b4793a51237bcb3c67ff24c933b14
Belmiro Sanguino Junior <juniorx_bjs@hotmail.com>
2018-04-09 22:37:01 -0400

    Entidade Produto Criada, criação do repository de produto, criacao das associações e correções do mapeamento da entidades e correção das inserções do banco

--- src/main/java/br/com/analise/compras/ComprasApplication.java
+++ src/main/java/br/com/analise/compras/ComprasApplication.java
@@ -1,7 +1,9 @@
 package br.com.analise.compras;
 
 import br.com.analise.compras.Entity.Categoria;
+import br.com.analise.compras.Entity.Produto;
 import br.com.analise.compras.repository.CategoriaRepository;
+import br.com.analise.compras.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,6 +19,8 @@ public class ComprasApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
+	@Autowired//para nao vim null
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComprasApplication.class, args);
@@ -24,18 +28,25 @@ public class ComprasApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... strings) throws Exception {
-		List<Categoria> categorias = new ArrayList<>();
-		Categoria categoria1 = new Categoria();
-		categoria1.setId(1);
-		categoria1.setNome("Peças");
 
-		Categoria categoria2 = new Categoria();
-		categoria2.setId(2);
-		categoria2.setNome("Alimento");
+		Categoria cat1= new Categoria(null,"informatica");
+		Categoria cat2= new Categoria(null,"Escritório");
 
-		categorias.add(categoria1);
-		categorias.add(categoria2);
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2 = new Produto(null,"impressora",800.00);
+		Produto p3 = new Produto(null,"mouse",80.00);
 
-		categoriaRepository.save(categorias);
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+
+
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
+		produtoRepository.save(Arrays.asList(p1,p2,p3));
+
 	}
 }
--- src/main/java/br/com/analise/compras/Entity/Categoria.java
+++ src/main/java/br/com/analise/compras/Entity/Categoria.java
@@ -1,22 +1,43 @@
 package br.com.analise.compras.Entity;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
-@Table (name = "TB_CATEGORIA")
+@Table(name = "TB_CATEGORIA")
+@SequenceGenerator(name = "seq_categoria",sequenceName = "seq_categoria")
 public class Categoria implements Serializable{
 
+
+
     @Id
-    @Column(name="CA_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "seq_categoria")
+    @Column(name= "CA_ID")
     private Integer id;
 
     @Column(name="CA_NOME")
     private String nome;
+
+    //associações
+    @JsonManagedReference
+    @ManyToMany(mappedBy = "categorias")//faça um mapeamento igual de categorias
+    private List<Produto>produtos=new ArrayList<>();//lista de produtos
+
+    public Categoria(){
+
+    }
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
 
     public String getNome() {
         return nome;
@@ -32,5 +53,27 @@ public class Categoria implements Serializable{
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
